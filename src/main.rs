045c689058dcc0c8dbc5163c1ca363774b2f62f8
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-08-28 22:54:32 -0500

    make preformance better

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,30 @@ use std::process::Command;
 use std::ptr::null;
 use std::thread::sleep;
 use std::time::Duration;
-use sysinfo::{ProcessorExt, SystemExt};
+use sysinfo::{ProcessorExt, System, SystemExt};
 use x11::xlib::{False, XDefaultRootWindow, XOpenDisplay, XStoreName, XSync};
 
 fn main() {
     unsafe {
         let display = XOpenDisplay(null());
+        let mut sys = System::new_all();
 
         loop {
             XStoreName(
                 display,
                 XDefaultRootWindow(display),
-                get_status().as_ptr() as *mut c_char,
+                get_status(&mut sys).as_ptr() as *mut c_char,
             );
             XSync(display, False);
-            sleep(Duration::from_millis(50));
+            sleep(Duration::from_millis(500));
         }
     }
 }
 
-fn get_status() -> CString {
-    let sys = sysinfo::System::new_all();
+fn get_status(sys: &mut System) -> CString {
+    sys.refresh_cpu();
+    sys.refresh_memory();
+
     let ram = (sys.used_memory() as f32 / sys.total_memory() as f32) * 100.;
     let swap = (sys.used_swap() as f32 / sys.total_swap() as f32) * 100.;
     let cpu = sys.global_processor_info().cpu_usage();
